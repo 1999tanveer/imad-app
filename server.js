95f0aa5de986bb2615023b62f167127f23a4bb83
1999tanveer <1999tanveer@gmail.com>
2017-08-08 22:43:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,21 +5,41 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var articleone = 
+var articles=
 {
-    title:'Article One|Tanveer Ahmad',
-    heading:'article one',
-    date:'8 Aug 2017',
-    content:`
-     <p>
-        This is my first web app. I am working on it...It gives a very good experience. Making our oun web apps is really amazing and gives a lot of pleasure.This is my first web app. I am working on it...It gives a very good experience. Making our oun web apps is really amazing and gives a lot of pleasure.
-    </p>
-     <p>
-        This is my first web app. I am working on it...It gives a very good experience. Making our oun web apps is really amazing and gives a lot of pleasure.This is my first web app. I am working on it...It gives a very good experience. Making our oun web apps is really amazing and gives a lot of pleasure.
-    </p>
+    article-one :{
+        title:'Article One|Tanveer Ahmad',
+        heading:'Article One',
+        date:'8 Aug 2017',
+        content:`
+         <p>
+            This is my first web app. I am working on it...It gives a very good experience. Making our oun web apps is really amazing and gives a lot of pleasure.This is my first web app. I am working on it...It gives a very good experience. Making our oun web apps is really amazing and gives a lot of pleasure.
+        </p>
+         <p>
+            This is my first web app. I am working on it...It gives a very good experience. Making our oun web apps is really amazing and gives a lot of pleasure.This is my first web app. I am working on it...It gives a very good experience. Making our oun web apps is really amazing and gives a lot of pleasure.
+        </p>
     `
+},
+    articl-two :{
+        title:'Article Two|Tanveer Ahmad',
+        heading:'Article Two',
+        date:'9 Aug 2017',
+        content:`
+         <p>
+            This is my second article.This is at the stage of development...
+        </p>`
+        },
+    article-three :{
+        title:'Article Three|Tanveer Ahmad',
+        heading:'Article Three',
+        date:'10 Aug 2017',
+        content:`
+         <p>
+            This is my third article.Stay tunned wih the article for latest updates.
+        </p>`
+        }
+    
 };
-
 
 function createTemplate(data){
     var title=data.title;
@@ -62,8 +82,8 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one', function (req, res) {
-  res.send(createTemplate(articleone));
+app.get('/:articleName', function (req, res) {
+  res.send(createTemplate(articles[articleName]));
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
