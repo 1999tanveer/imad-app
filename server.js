851dbfa9bf9e99b115221042adb0744b9087d0a6
1999tanveer <1999tanveer@gmail.com>
2017-08-08 19:56:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,9 +5,55 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleone = 
+{
+    title:'Article One|Tanveer Ahmad',
+    heading:'article one',
+    date:'8 Aug 2017',
+    content:`
+     <p>
+                This is my first web app. I am working on it...It gives a very good experience. Making our oun web apps is really amazing and gives a lot of pleasure.This is my first web app. I am working on it...It gives a very good experience. Making our oun web apps is really amazing and gives a lot of pleasure.
+            </p>
+             <p>
+                This is my first web app. I am working on it...It gives a very good experience. Making our oun web apps is really amazing and gives a lot of pleasure.This is my first web app. I am working on it...It gives a very good experience. Making our oun web apps is really amazing and gives a lot of pleasure.
+    `
+};
+
+var htmltemplate=
+{
+ <html>
+    <head>
+        <title>
+               ${title}
+        </title>
+        <meta name="viewport" content="width-device-width, initial-scale=1"/>
+        <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+
+    
+    <body class="container">
+        <div>
+            <a href="/">Home</a>
+        </div>
+        <hr/>
+        
+        <h3>
+            ${heading}
+        </h3>
+        <div>
+            ${date}
+        </div>
+        <div>
+            ${content}
+        </div>
+    </body>
+    
+</html>   
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
+}
 
 app.get('/article-one', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
