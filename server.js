85139f4b66668b2b7d7c6fa1c59688cadf34562c
1999tanveer <1999tanveer@gmail.com>
2017-08-09 07:35:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -80,7 +80,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/:articleName', function (req, res) {
+app.get('/articleone', function (req, res) {
   res.send(createTemplate(articles[articleone]));
 });
 app.get('/ui/style.css', function (req, res) {
