df0e75e9329eefa63e4662eee23797d8cfcca99c
1999tanveer <1999tanveer@gmail.com>
2017-08-12 16:11:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ var articles={
 };   
 
 function createTemplate(data){
-    var title =data.title;
+    //var title =data.title;
     var heading=data.heading;
     var date=data.date;
     var content=data.content;
@@ -48,7 +48,7 @@ function createTemplate(data){
      <html>
         <head>
             <title>
-                   ${title}
+                   ${data.title}
         </title>
             <meta name="viewport" content="width-device-width, initial-scale=1"/>
             <link href="/ui/style.css" rel="stylesheet" />
